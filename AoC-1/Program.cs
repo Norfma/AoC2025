3eda56b5015e33c468e487bff25b2c69e0ed5a5f
lhernoux <lhernoux@pictet-technologies>
2025-12-10 15:43:03 +0100

    Solution-AoC1

--- AoC-1/Program.cs
+++ AoC-1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string filePath = "/Users/lhernoux/MyGithubProjects/AoC2025/AoC-1/input.csv";
+Console.WriteLine($"Reading file: {filePath}");
+List<(char Direction, int Distance)> commands = [];
+
+if (File.Exists(filePath))
+{
+    Console.WriteLine("Number of lines in file: " + File.ReadAllLines(filePath).Length);
+    foreach (string line in File.ReadAllLines(filePath))
+    {
+        if (string.IsNullOrWhiteSpace(line)) continue;
+        
+        char direction = line[0]; // First character: 'R' or 'L'
+        if (int.TryParse(line.Substring(1), out int distance))
+        {
+            commands.Add((direction, distance));
+        }
+    }
+}
+
+int pointer = 50;
+int numberOfZero = 0;
+foreach (var (dir,dist) in commands)
+{
+    if (dir == 'R')
+    {
+        pointer += dist;
+    }
+    else if (dir == 'L')
+    {
+        pointer -= dist;
+    }
+    pointer = pointer % 100;
+    if (pointer == 0) numberOfZero++;
+}
+Console.WriteLine($"\nFinal pointer position: {pointer}");
+Console.WriteLine($"Number of times pointer was at 0: {numberOfZero}");
